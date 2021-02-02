d98c0f718721bed0b5e1938a6c2cddc659172bbd
philemonheng <phil.hsy@outlook.com>
2021-02-03 00:07:06 +0800

    added navbar and removed zoom control

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-var mymap = L.map('mapid').setView([1.3521, 103.8198], 11);
+var mymap = L.map('mapid', { zoomControl: false }).setView([1.323, 103.8198], 12);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    minZoom: 12,
     id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZmlsbGluZ3RoZW1vb24iLCJhIjoiY2tpaGxrbjZmMDNicTJ4bThmd3preWJvbyJ9.ca_X_SaaPktyuEBL2RzoRA'
 }).addTo(mymap);
+
+mymap.setMaxBounds(mymap.getBounds());
