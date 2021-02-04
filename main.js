5f1b803cf9628278941312026f9731fb8e891b6c
philemonheng <phil.hsy@outlook.com>
2021-02-04 17:19:06 +0800

    add writeup to geojson

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function populateInfoPanel(e) {
   header.textContent = e.target.feature.properties.farm;
   var paragraph = document.createElement("p");
   //paragraph.textContent = e.target.feature.properties.address;
-  paragraph.textContent = 'iaugue lacus viverra vitae congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh augue lacus viverra vitae congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh';
+  paragraph.textContent = e.target.feature.properties.writeup;
   infoPanelContent.appendChild(header);
   infoPanelContent.appendChild(paragraph);
 
